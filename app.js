1b7e0553d8f365c055a0fda31a2d717af13bee28
Kamlesh Mistry <mistrykamlesh312@gmail.com>
2020-07-09 17:12:30 +0530

    Heroku remote

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ app.use(bodyParser.urlencoded({extended: true}));
 
 app.use(express.static('public'));
 
-mongoose.connect('mongodb://localhost:27017/todolistDB',{useNewUrlParser: true, useUnifiedTopology: true});
+mongoose.connect('mongodb+srv://admin-kamlesh:mistry@312@cluster0.tbtr6.mongodb.net/todolistDB',{useNewUrlParser: true, useUnifiedTopology: true});
 
 const itemsSchema = new mongoose.Schema({
     name: String
